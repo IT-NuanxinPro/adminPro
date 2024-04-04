e3ab35f51ea69071ff86acce55f3117f7e5054f1
IT-NuanxinPro <IT-NuanxinPro@users.noreply.github.com>
2024-04-04 08:32:05 +0000

    自动部署 a969ba32a690ff60fb0cd6eff475ad0877d762ef

--- README.md
+++
@@ -1,2 +0,0 @@
-# adminPro
-从0-1 搭建vue3 PC后台管理系统
---
+++ static/js/index-BeMv_XGZ.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,o as t,_ as n}from"./index-Bo_4x6aL.js";const s={class:"container"},c=e({__name:"index",setup(_){return(a,r)=>(t(),o("div",s," hello world "))}}),p=n(c,[["__scopeId","data-v-81716915"]]);export{p as default};
